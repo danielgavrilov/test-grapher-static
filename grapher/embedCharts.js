21c571ee5ed0a7e24c35fb0156a1bbec29b7e32a
OWID <owid@ourworldindata.org>
2018-12-05 19:54:51 +0000

    All the things

---
+++ grapher/embedCharts.js
@@ -0,0 +1,39 @@
+
+        window.App = {};
+        window.Global = { rootUrl: 'https://mispytest-grapher.netlify.com/grapher' };
+
+        var link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = 'https://mispytest-grapher.netlify.com/grapher/assets/commons.bundle.2e22f5e69cab1ba9e613.css';
+        document.head.appendChild(link);
+
+        var hasPolyfill = false;
+        var hasGrapher = false;
+
+        var loadedScripts = 0;
+        function checkReady() {
+            loadedScripts += 1;
+            if (loadedScripts == 3) {
+                window.Grapher.embedAll();
+            }
+        }
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = checkReady;
+        script.src = "https://cdn.polyfill.io/v2/polyfill.min.js?features=es6,fetch"
+        document.head.appendChild(script);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = checkReady;
+        script.src = 'https://mispytest-grapher.netlify.com/grapher/assets/commons.bundle.2e22f5e69cab1ba9e613.js';
+        document.head.appendChild(script);
+
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.onload = checkReady;
+        script.src = 'https://mispytest-grapher.netlify.com/grapher/assets/charts.bundle.2e22f5e69cab1ba9e613.js';
+        document.head.appendChild(script);
+    
